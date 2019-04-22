275f829d859679730c6af68d16a84affd8c6bca9
Eddie Chu <contact@eddiechu.me>
2019-04-21 18:20:39 -0700

    cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Thirder = require('./stringSamples')
 app.use(express.json());
 app.use(
     cors({
-        origin: ['*']
+        origin: ['https://lyft-technical-sample.herokuapp.com/']
     })
 );
 
